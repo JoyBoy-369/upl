2f5a183ddaf629c8f3533055b3a88e5a0b637f37
kevin n <kevin.nin92@gmail.com>
2024-04-27 12:59:47 +0530

    fix vercel errors

---
+++ .vercel/output/functions/_render.func/Desktop/workspace/projects/websites/upl/.vercel/output/_functions/chunks/generic_4a678ccf.mjs
@@ -0,0 +1,6 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+
+const page = () => import('./pages/generic_fb429a85.mjs').then(n => n.d);
+
+export { page };
