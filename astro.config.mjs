3f0dbe8787230ed16b86775c3873245bd5880206
kevin n <kevin.nin92@gmail.com>
2024-04-27 13:05:41 +0530

    added domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
+  site: "https://upl-xi.vercel.app",
   integrations: [tailwind(), mdx(), sitemap(), react()],
   output: "server",
   adapter: vercel(),
