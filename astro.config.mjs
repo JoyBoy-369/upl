043f5e5edc74443d0fec7bf30c87b71d69145cb7
kevin n <kevin.nin92@gmail.com>
2024-04-27 12:52:10 +0530

    added vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,15 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astroship.web3templates.com",
-  integrations: [tailwind(), mdx(), sitemap(), react()]
+  integrations: [tailwind(), mdx(), sitemap(), react()],
+  output: "server",
+  adapter: vercel(),
 });
+
